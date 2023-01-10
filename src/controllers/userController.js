a4b2756a1788397ed167ca8acdba122b2b8d29e4
Dark-Milton <hegdesumanth8@gmail.com>
2023-01-11 00:11:45 +0530

    Initial Commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,44 +8,221 @@ exports.userLogin =  (req, res) => {
     const userName = req.body.name
     const userEmail = req.body.email
     const userPassword = req.body.password
+    const newPassword = req.body.newPasswd
+    let age = req.body.age
+    let gender = req.body.gender
+    let location = req.body.location
+    const updateFlag = req.body.update
     console.log(req.body)
-    connection.query(`select * from users where Email = "${userEmail}"`, async (err, results, field) => {
-        if (err) {
-            res.status(500).send({
-                "message": "Server error"
-            })
-            console.log(err)
-        } else {
-            if (results.length == 0) {
-                return res.status(401).send({
-                    "message": "The entered credentials do not match"
+    if(updateFlag == undefined) {
+        connection.query(`select * from users where Email = "${userEmail}"`, async (err, results, field) => {
+            if (err) {
+                res.status(500).send({
+                    "message": "Server error"
                 })
-            }
-            // const match = await bcrypt.compare(userPassword, results[0].password)
-            console.log(results)
-            match = (userPassword == results[0].Password) && (userName == results[0].Name) && (userEmail == results[0].Email) 
-            if (match) {
-                // req.session.userName = results[0].Name
-                // res.status(200).send()
-                res.render('userdash.hbs', {
-                    "user": results[0]
-                })
+                console.log(err)
             } else {
-                res.status(401).send({
-                    "message": "The entered credentials do not match"
+                if (results.length == 0) {
+                    return res.status(401).send({
+                        "message": "The entered credentials do not match"
+                    })
+                }
+                // const match = await bcrypt.compare(userPassword, results[0].password)
+                console.log(results)
+                match = (userPassword == results[0].Password) && (userName == results[0].Name) && (userEmail == results[0].Email) 
+                if (match) {
+                    // req.session.userName = results[0].Name
+                    // res.status(200).send()
+                    res.render('userdash.hbs', {
+                        "user": results[0]
+                    })
+                } else {
+                    res.status(401).send({
+                        "message": "The entered credentials do not match"
+                    })
+                }
+            }
+        })    
+    }
+    else if(updateFlag == 0) {
+        connection.query(`Select * from users where Email = "${userEmail}"`, (err, results, fields) => {
+            if (err) {
+                res.status(500).send({
+                    "message": "Server error"
+                })
+                console.log(err)
+            } else {
+                if (results.length == 0) {
+                    return res.status(401).send({
+                        "message": "The entered credentials do not match"
+                    })
+                }
+                // const match = await bcrypt.compare(userPassword, results[0].password)
+                console.log(results)
+                match = (userPassword == results[0].Password)
+                if (match) {
+                    // req.session.userName = results[0].Name
+                    // res.status(200).send()
+                    connection.query('UPDATE users SET Password = ? WHERE Email = ?',[newPassword,userEmail], (error, userresults, fields) => {
+                        if(error) {
+                            console.log(error)
+                            res.status(500).send({
+                                "message": "Server error"
+                            })
+                        }
+                        else {
+                            connection.query(`select * from users where Email = "${userEmail}"`, async (err, results, field) => {
+                                if (err) {
+                                    res.status(500).send({
+                                        "message": "Server error"
+                                    })
+                                    console.log(err)
+                                } else {
+                                    if (results.length == 0) {
+                                        return res.status(401).send({
+                                            "message": "The entered credentials do not match"
+                                        })
+                                    }
+                                    res.render('userdash.hbs', {
+                                        "user": results[0]
+                                    })
+                                }
+                            }) 
+                        }
+                    })
+                }
+                else{
+                    return res.status(401).send({
+                        "message": "The entered credentials do not match"
+                    })
+                }
+            }
+        })
+    }
+    else if(updateFlag == 1){
+        connection.query(`Select * from users where Email = '${userEmail}'`, async (err, results, fields) => {
+            if (err) {
+                res.status(500).send({
+                    "message": "Server error"
+                })
+                console.log(err)
+            } else {
+                // const match = await bcrypt.compare(userPassword, results[0].password)
+                console.log(results)
+                console.log(age, gender, location)
+                if(age == '')
+                    age = results[0].Age
+                if(gender == '')
+                    gender = results[0].Gender
+                if(location == '')
+                    location = results[0].Location
+                console.log(age, gender, location)
+                connection.query('UPDATE users SET Age = ?, Gender = ?, Location = ? WHERE Email = ?', [age, gender, location, userEmail], (err, results, field) => {
+                    if (err) {
+                        console.log(err)
+                        res.status(500).send({
+                            "message": "Server error"
+                        })
+                    } 
+                    else {
+                        connection.query(`select * from users where Email = "${userEmail}"`, async (err, results, field) => {
+                        if (err) {
+                            res.status(500).send({
+                                "message": "Server error"
+                            })
+                            console.log(err)
+                        } else {
+                            if (results.length == 0) {
+                                return res.status(401).send({
+                                    "message": "The entered credentials do not match"
+                                })
+                            }
+                            res.render('userdash.hbs', {
+                                "user": results[0]
+                            })
+                        }
+                    }) 
+                    }
                 })
             }
-        }
-    })    
+        })
+    }
+    else {
+        connection.query(`select * from users where Email = "${userEmail}"`, async (err, results, field) => {
+            if (err) {
+                res.status(500).send({
+                    "message": "Server error"
+                })
+                console.log(err)
+            } else {
+                console.log(results)
+                if (results.length == 0) {
+                    return res.status(401).send({
+                        "message": "The entered credentials do not match"
+                    })
+                }
+                // const match = await bcrypt.compare(userPassword, results[0].password)
+                match = (userPassword == results[0].Password) && (userEmail == results[0].Email) 
+                if (match) {
+                    // req.session.userName = results[0].Name
+                    // res.status(200).send()
+                    res.render('userdash.hbs', {
+                        "user": results[0]
+                    })
+                } else {
+                    res.status(401).send({
+                        "message": "The entered credentials do not match"
+                    })
+                }
+            }
+        })
+    }
 };
+// exports.userLogin =  (req, res) => {
+//     const userName = req.body.name
+//     const userEmail = req.body.email
+//     const userPassword = req.body.password
+//     console.log(req.body)
+//     connection.query(`select * from users where Email = "${userEmail}"`, async (err, results, field) => {
+//         if (err) {
+//             res.status(500).send({
+//                 "message": "Server error"
+//             })
+//             console.log(err)
+//         } else {
+//             if (results.length == 0) {
+//                 return res.status(401).send({
+//                     "message": "The entered credentials do not match"
+//                 })
+//             }
+//             // const match = await bcrypt.compare(userPassword, results[0].password)
+//             console.log(results)
+//             match = (userPassword == results[0].Password) && (userName == results[0].Name) && (userEmail == results[0].Email) 
+//             if (match) {
+//                 // req.session.userName = results[0].Name
+//                 // res.status(200).send()
+//                 res.render('userdash.hbs', {
+//                     "user": results[0]
+//                 })
+//             } else {
+//                 res.status(401).send({
+//                     "message": "The entered credentials do not match"
+//                 })
+//             }
+//         }
+//     })    
+// };
 exports.userSignUp =  async (req, res) => {
     const userName = req.body.name
     const userEmail = req.body.email
     const userPassword = req.body.password
     const userGroup = req.body.group
     const userPhNo = req.body.PhNo
+    const userAge = req.body.age
+    const userGender = req.body.gender
+    const userLocation = req.body.location
     const encryptedPassword = userPassword
-    connection.query(`insert into users(Name, Email, Bgroup, PhNo, Password) Values ("${userName}", "${userEmail}", "${userGroup}", "${userPhNo}", "${encryptedPassword}")`, (err, results, field) => {
+    connection.query(`insert into users(Name, Email, Bgroup, PhNo, Age, Gender, Location, Password) Values ("${userName}", "${userEmail}", "${userGroup}", "${userPhNo}", "${userAge}", "${userGender}", "${userLocation}", "${encryptedPassword}")`, (err, results, field) => {
         if (err) {
             console.log(err)
             if (err.errno === 1062) {
@@ -147,10 +324,36 @@ exports.unregister = async (req, res) => {
 };
 
 exports.view = (req, res) => {
-    console.log(req.params.email)
-    res.render('changePasswd',
-     {email: req.params.email})
+    const email = req.body.email
+    const password = req.body.password
+    connection.query(`Select Password from users where Email = '${email}'`, async (err, results, fields) => {
+        if (err) {
+            res.status(500).send({
+                "message": "Server error"
+            })
+            console.log(err)
+        } else {
+            console.log(results)
+            if (results.length == 0) {
+                res.redirect('/')
+            }
+            if(password == results[0].Password) {
+                res.render('changePasswd.hbs', {
+                    'email': email,
+                    'password': password
+                })
+            }
+            else {
+                res.redirect('/')
+            }
+        }
+    })
 };
+// exports.view = (req, res) => {
+//     console.log(req.params.email)
+//     res.render('changePasswd',
+//      {email: req.params.email})
+// };
 exports.changePasswd = (req, res) => {
     const oldPasswd = req.body.oldPasswd
     const newPasswd = req.body.newPasswd
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,57 +8,41 @@ router.post('/api/userSignUp', userController.userSignUp)
 router.post('/api/bloodBankLogin', userController.bloodBankLogin)
 router.post('/api/bloodBankSignUp', userController.bloodBankSignUp)
 router.post('/api/unregister', userController.unregister)
-router.get('/changePasswd/:email', userController.view)
-router.post('/changePasswd/', userController.changePasswd)
-router.get('/editInfo/:email', async (req, res) => {
-    const Email = req.params.email
-    res.render('editInfo.hbs', {
-        email: Email
-    })
-})
+router.post('/changePasswd', userController.view)
 router.post('/editInfo', async (req, res) => {
     const email = req.body.email
-    let age = req.body.age
-    let gender = req.body.gender
-    let location = req.body.location
-    // res.sendStatus(200)
-    connection.query(`Select * from users where Email = '${email}'`, async (err, results, fields) => {
+    const password = req.body.password
+    connection.query(`Select Password from users where Email = '${email}'`, async (err, results, fields) => {
         if (err) {
             res.status(500).send({
                 "message": "Server error"
             })
             console.log(err)
         } else {
-            // const match = await bcrypt.compare(userPassword, results[0].password)
             console.log(results)
-            console.log(age, gender, location)
-            if(age == '')
-                age = results[0].Age
-            if(gender == '')
-                gender = results[0].Gender
-            if(location == '')
-                location = results[0].Location
-            console.log(age, gender, location)
-            connection.query('UPDATE users SET Age = ?, Gender = ?, Location = ? WHERE Email = ?', [age, gender, location, email], (err, results, field) => {
-                if (err) {
-                    console.log(err)
-                    res.status(500).send({
-                        "message": "Server error"
-                    })
-                } 
-                else {
-                    const finish = () => {
-                        res.redirect('/userlogin')
-                    }
-                    setTimeout(finish, 2000)
-                }
-            })
+            if (results.length == 0) {
+                res.redirect('/')
+            }
+            if(password == results[0].Password) {
+                res.render('editInfo.hbs', {
+                    'email': email,
+                    'password': password
+                })
+            }
+            else {
+                res.redirect('/')
+            }
         }
     })
 })
-router.get('/dashboard', (req, res) => {
-    res.redirect('../')
-})
+
+// router.get('/dashboard/:email', async (req, res) => {
+//     const finish = () => {
+//         console.log("done")
+//     }
+//     await setInterval(finish, 3000)
+//     res.redirect('../')
+// })
 router.get('/logout', (req, res) => {
     res.render('')
 })
