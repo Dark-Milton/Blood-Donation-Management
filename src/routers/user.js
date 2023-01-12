0b14192016523b4c76d8f81ddd3013941fa0025d
Dark-Milton <hegdesumanth8@gmail.com>
2023-01-12 23:47:39 +0530

    Initial Commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,12 +4,16 @@ const {connection, mysql} = require('../db/sql')
 const userController = require('../controllers/userController');
 
 router.get('', (req, res) => {
-    connection.query(`select Name, Bgroup from users LIMIT 7`, async (err, results, field) => {
+    connection.query(`select Name, Bgroup from users LIMIT 7`, async (err, result, field) => {
+        connection.query(`select * from faq`, async (err, results, field) => {
             res.render('index.hbs', {
-                result: results
+                'result': result,
+                'faq': results
             })
-    }) 
-})
+        }) 
+    })
+}) 
+
 
 router.post('/api/userLogin', userController.userLogin)
 router.post('/api/userSignUp', userController.userSignUp)
